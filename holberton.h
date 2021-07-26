cd91206e799d0d73d786307680f7780a4cc874c9
MsMutanu <faithtanuh@gmail.com>
2021-07-26 21:39:11 +0300

    Header file

---
+++ holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+  *struct directives - holds fromat specifiers and their functions.
+  *@spec: char.
+  *@fspec: function pointer.
+  *
+  */
+typedef struct directives
+{
+	char spec;
+	int (*fspec)(va_list, int);
+} forms;
